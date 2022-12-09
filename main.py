93a85bd557e55a102c0b64e4fbec183a2ec30904
AliyaAIS21 <zhnyak.orion@bk.ru>
2022-12-09 13:09:59 +0500

    таблица с кнопками

--- main.py
+++ main.py
@@ -21,3 +21,46 @@ Job varchar(30),
 Status_Job varchar(30))
 """)
 conn.commit()
+
+#Главное окно
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    def initUI(self):
+        self.setGeometry(700, 100, 700, 700)
+        self.setWindowTitle('Планировщик на день')
+        self.spisokt = QLabel('Список дел', self)
+        self.spisokt.move(300, 30)
+
+        self.db = QSqlDatabase.addDatabase("QSQLITE")
+        self.db.setDatabaseName("planirovshik.sqlite")
+        self.db.open()
+        self.view = QTableView(self)
+        self.model = QSqlTableModel(self, self.db)
+        self.model.setTable('Plan')
+        self.model.select()
+        self.view.setModel(self.model)
+        self.view.move(50, 60)
+        self.view.resize(600, 300)
+
+        self.button_1 = QPushButton(self)
+        self.button_1.move(245, 550)
+        self.button_1.resize(170, 30)
+        self.button_1.setText("Добавить дело")
+        #self.button_1.clicked.connect(self.run2)
+
+        self.button_2 = QPushButton(self)
+        self.button_2.move(245, 600)
+        self.button_2.resize(170, 30)
+        self.button_2.setText("Удалить дело")
+        # self.button_1.clicked.connect(self.run2)
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
